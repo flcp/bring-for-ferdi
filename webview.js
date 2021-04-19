f97cf8ea820e277a36d56ee64e78b0cd349a1dfe
Florian Pohl <florian.pohl@andrena.de>
2021-04-19 17:58:33 +0200

    adds showing number of articles in current list as badge

--- webview.js
+++ webview.js
@@ -1,2 +1,13 @@
-module.exports = (Ferdi) => {
-}
+const path = require('path');
+
+module.exports = Franz => {
+  const getNumberOfElements = function getNumberOfElements() {
+	var badge = document.getElementsByClassName("bring-list-selector-list-item-count selected")[0];
+	var count = badge.innerText;
+
+    Franz.setBadge(count);
+  };
+
+  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Franz.loop(getNumberOfElements);
+};
