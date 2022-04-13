0f4022e1d435bb6c4135b6b73835d2f02b25721c
Florian Pohl <florian.pohl.ext@siemens.com>
2022-04-13 15:29:48 +0200

    remove badge number

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# bring-for-ferdi
+# Bring for ferdi
 Integration of Bring Web App into [Ferdi](https://getferdi.com)
 
 Usage:
 Copy contents of Repository into "%appdata%/Roaming/Ferdi/recipes/dev/bring".
 Restart Ferdi to be able to add custom Service Bring!
-
-Currently shows a badge (i.e. red number, usually used to indicate unread messages) with its number equal to the number of article items in the currently selected shopping list.
--- webview.js
+++ webview.js
@@ -1,13 +1,5 @@
 const path = require('path');
 
-module.exports = Franz => {
-  const getNumberOfElements = function getNumberOfElements() {
-	var badge = document.getElementsByClassName("bring-list-selector-list-item-count selected")[0];
-	var count = badge.innerText;
-
-    Franz.setBadge(count);
-  };
-
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
-  Franz.loop(getNumberOfElements);
+module.exports = ferdi => {
+  ferdi.injectCSS(path.join(__dirname, 'service.css'));
 };
